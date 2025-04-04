6938eddfe07aefd79874e03ae1a3ce6c252f9e47
Deepak Bhattarai <108481783+DevDeepakBhattarai@users.noreply.github.com>
2025-04-04 08:05:21 +0545

    changed the db file

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const createPrismaClient = () =>
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
+  });
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
